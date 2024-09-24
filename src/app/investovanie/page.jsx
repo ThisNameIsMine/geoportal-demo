90321f60d4aa304ce2f49331b8c54e429ce4d606
Filip Katušin <filip.katusin@student.tuke.sk>
2024-09-24 11:29:33 +0200

    veci

--- src/app/investovanie/page.jsx
+++ src/app/investovanie/page.jsx
@@ -5,23 +5,37 @@ import { useRouter } from "next/navigation";
 
 const slides = [
   {
-    title: "Kto ste",
+    title: "Kto ste?",
     questions: ["Firma", "Investor", "Startup", "Iny"],
     type: "radio",
   },
   {
     title: "Prečo chcete investovať?",
-    questions: ["neico", "nieco", "nieco", "lebo mam lovaky"],
+    questions: [
+      "Kraj ma zaujal",
+      "Dostupné investičné prostriedky",
+      "Kvalifikovaná pracovná sila",
+      "Rastúci cestovný ruch",
+    ],
     type: "checkbox",
   },
   {
-    title: "Kolko mate penazi?",
+    title: "Koľko mate finančných prostriedkov?",
     questions: ["menej ako 10k", "10k - 100k", "100k - 1M", "1M a viac"],
     type: "radio",
   },
   {
-    title: "Nadpis text daco...",
-    questions: ["menej ako 10k", "10k - 100k", "100k - 1M", "1M a viac"],
+    title: "Aký je váš investičný horizont?",
+    questions: ["1-3 roky", "3-5 rokov", "5-10 rokov", "10+ rokov"],
+    type: "checkbox",
+  },
+  {
+    title: "Aké faktory vás môžu odradiť od investície do tohto projektu?",
+    questions: [
+      "Nedostatočné údaje a transparentnosť",
+      "Vysoké riziko a neistota",
+      "Nedostatok lokálnej podpory a spolupráce",
+    ],
     type: "checkbox",
   },
 ];
@@ -34,7 +48,7 @@ const Page = () => {
   const handleNextSlide = () => {
     setActiveSlide((prev) => prev + 1);
 
-    if (activeSlide + 1 === slides.length) {
+    if (activeSlide === slides.length) {
       setTimeout(() => {
         router.push("/investovanie/ponuky");
       }, 2000);
@@ -43,7 +57,7 @@ const Page = () => {
 
   return (
     <div className="flex justify-center items-center h-screen bg-primary">
-      {activeSlide < slides.length ? (
+      {activeSlide <= slides.length ? (
         <>
           {slides.map((slide, index) => (
             <Box
@@ -53,224 +67,49 @@ const Page = () => {
               onNextClick={handleNextSlide}
             />
           ))}
+
+          <div
+            className={`w-1/2 min-h-1/2 bg-secondary rounded-lg flex flex-col p-8 absolute ${
+              activeSlide < slides.length ? "hidden" : ""
+            }`}
+          >
+            <div className="flex-1">
+              <div className="text-white text-3xl text-center font-semibold mb-4">
+                Kontaktne údaje
+              </div>
+
+              <div className="flex flex-col gap-2 items-center">
+                <input
+                  type="text"
+                  placeholder="Meno / Názov firmy"
+                  className="input input-bordered w-full max-w-xs bg-gray-700 text-white"
+                />
+
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="input input-bordered w-full max-w-xs bg-gray-700"
+                />
+              </div>
+            </div>
+            <div className="flex justify-end mt-4">
+              <button
+                className="bg-tertiary py-2 px-4 hover:bg-hover font-bold text-white rounded text-xl"
+                onClick={handleNextSlide}
+              >
+                Ďalej
+              </button>
+            </div>
+          </div>
         </>
       ) : (
         <div className="flex flex-col gap-10 items-center">
           <p className="text-4xl text-center text-white">
-            vyhodnocujem vase data
+            Vaše odpovede sa spracovávajú
           </p>
           <span className="loading loading-dots loading-lg text-tertiary"></span>
         </div>
       )}
-    </div>
-  );
-
-  return (
-    <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content text-center">
-          <div className="max-w-sm md:max-w-xl">
-            <h1 className="text-2xl font-bold mb-6">Čo Vás zaujíma?</h1>
-
-            {/* Form starts here */}
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div className="flex gap-20">
-                {/* Name field */}
-                <div>
-                  <label className="block text-left mb-2 font-semibold">
-                    Meno:
-                  </label>
-                  <input
-                    type="text"
-                    name="Meno"
-                    value={formData.name}
-                    onChange={handleChange}
-                    placeholder="Zadajte meno"
-                    className="input input-bordered w-full"
-                    required
-                  />
-                </div>
-
-                {/* Email field */}
-                <div>
-                  <label className="block text-left mb-2 font-semibold">
-                    Email:
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    placeholder="Zadajte email"
-                    className="input input-bordered w-full"
-                    required
-                  />
-                </div>
-              </div>
-
-              <div className="flex gap-20">
-                {/* Phone Number field */}
-                <div>
-                  <label className="block text-left mb-2 font-semibold">
-                    Tel. číslo:
-                  </label>
-                  <input
-                    type="tel"
-                    name="phone"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    placeholder="Zadajte tel. číslo"
-                    className="input input-bordered w-full"
-                    required
-                  />
-                </div>
-
-                {/* country */}
-                <div>
-                  <label className="block text-left mb-2 font-semibold">
-                    Krajina:
-                  </label>
-                  <input
-                    type="country"
-                    name="country"
-                    value={formData.phone}
-                    onChange={handleChange}
-                    placeholder="Zadajte krajinu"
-                    className="input input-bordered w-full"
-                    required
-                  />
-                </div>
-              </div>
-
-              {/* Sector dropdown */}
-              <div>
-                <label className="block text-left mb-2 font-semibold">
-                  Sektor:
-                </label>
-                <select
-                  name="sector"
-                  value={formData.sector}
-                  onChange={handleChange}
-                  className="select select-bordered w-full"
-                  required
-                >
-                  <option value="" disabled>
-                    Vyberte sektor
-                  </option>
-                  <option value="transport">Transport</option>
-                  <option value="health">Zdravotníctvo</option>
-                  <option value="nature">Príroda</option>
-                  <option value="living">Bývanie</option>
-                  <option value="education">Vzdelanie</option>
-                  <option value="energy">Energetika</option>
-                  <option value="farming">Poľnohospodárstvo</option>
-                  <option value="industry">Priemysel</option>
-                  <option value="finance">Finančníctvo</option>
-                  <option value="tourism">Cestovný Ruch</option>
-                  <option value="Economy">Economika</option>
-                </select>
-              </div>
-
-              {/* Budget dropdown */}
-              <div>
-                <label className="block text-left mb-2 font-semibold">
-                  Rozpočet:
-                </label>
-                <select
-                  name="budget"
-                  value={formData.budget}
-                  onChange={handleChange}
-                  className="select select-bordered w-full"
-                  required
-                >
-                  <option value="" disabled>
-                    Vyberte rozpočet
-                  </option>
-                  <option value="10000">10,000 a menej</option>
-                  <option value="100000">100,000 a menej</option>
-                  <option value="1000000">1,000,000 a menej</option>
-                </select>
-              </div>
-
-              {/* Budget dropdown */}
-              <div>
-                <label className="block text-left mb-2 font-semibold">
-                  Etapa investície:
-                </label>
-                <select
-                  name="stage"
-                  value={formData.budget}
-                  onChange={handleChange}
-                  className="select select-bordered w-full"
-                  required
-                >
-                  <option value="" disabled>
-                    Vyberte etapu
-                  </option>
-                  <option value="early">Skorý Startup</option>
-                  <option value="seriea">Seria A</option>
-                  <option value="serieb">Seria B a ďalej</option>
-                  <option value="late">Neskorá etapa spoločnosti</option>
-                  <option value="m&a">M&A</option>
-                  <option value="realestate">Nehnuteľnosti</option>
-                  <option value="venture">Venture Capital</option>
-                  <option value="IPO">IPO</option>
-                </select>
-              </div>
-
-              {/* Budget dropdown */}
-              <div>
-                <label className="block text-left mb-2 font-semibold">
-                  Risk:
-                </label>
-                <select
-                  name="risk"
-                  value={formData.budget}
-                  onChange={handleChange}
-                  className="select select-bordered w-full"
-                  required
-                >
-                  <option value="" disabled>
-                    Vyberte risk
-                  </option>
-                  <option value="low">Nízky</option>
-                  <option value="medium">Stredný</option>
-                  <option value="high">Vysoký</option>
-                </select>
-              </div>
-
-              {/* Budget dropdown */}
-              <div>
-                <label className="block text-left mb-2 font-semibold">
-                  Typ účasti:
-                </label>
-                <select
-                  name="involvement"
-                  value={formData.budget}
-                  onChange={handleChange}
-                  className="select select-bordered w-full"
-                  required
-                >
-                  <option value="" disabled>
-                    Vyberte typ účasti
-                  </option>
-                  <option value="handsoff">Hands-off</option>
-                  <option value="advisory">Poradenská</option>
-                  <option value="active">Aktívna účasť</option>
-                </select>
-              </div>
-
-              {/* Submit button */}
-              <div>
-                <button type="submit" className="btn btn-primary w-full">
-                  Potvrdiť
-                </button>
-              </div>
-            </form>
-            {/* Form ends here */}
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
@@ -286,7 +125,7 @@ const Box = ({ slide, hidden, onNextClick }) => {
     >
       <div className="flex-1">
         <div className="text-white text-3xl text-center font-semibold mb-4">
-          {slide.title},
+          {slide.title}
         </div>
 
         <ul className="flex flex-col gap-2">
--- src/app/investovanie/ponuky/[id]/page.jsx
+++ src/app/investovanie/ponuky/[id]/page.jsx
@@ -1,69 +1,102 @@
 import React from "react";
+import { investmentOpportunities } from "../items";
 
 const DetilPage = ({ params: { id } }) => {
+  const investmentOpportunity = investmentOpportunities[id];
+
   return (
-    <div className="bg-primary min-h-screen grid grid-cols-2">
-      <div>
-        <div className="carousel w-full">
-          <div id="slide1" className="carousel-item relative w-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-              className="w-full"
-            />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href="#slide4" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide2" className="btn btn-circle">
-                ❯
-              </a>
+    <div className="bg-primary min-h-screen">
+      <div className="container mx-auto py-20 grid grid-cols-2 gap-8">
+        <div>
+          <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Truck.car.transporter.arp.750pix.jpg"
+                className="w-full"
+              />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide4" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide2" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
             </div>
-          </div>
-          <div id="slide2" className="carousel-item relative w-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-              className="w-full"
-            />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href="#slide1" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide3" className="btn btn-circle">
-                ❯
-              </a>
+            <div id="slide2" className="carousel-item relative w-full">
+              <img
+                src="https://i.pinimg.com/736x/b8/55/01/b85501f55f5104f876ffb5c5baf35957.jpg"
+                className="w-full"
+              />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide1" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide3" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
             </div>
-          </div>
-          <div id="slide3" className="carousel-item relative w-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-              className="w-full"
-            />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href="#slide2" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide4" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide4" className="carousel-item relative w-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-              className="w-full"
-            />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href="#slide3" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide1" className="btn btn-circle">
-                ❯
-              </a>
+            <div id="slide3" className="carousel-item relative w-full">
+              <img
+                src="https://www.portseurope.com/wp-content/uploads/2020/02/Railways.jpg"
+                className="w-full"
+              />
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide2" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide4" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
             </div>
           </div>
         </div>
+        <div className="text-white">
+          <h1 className="text-white font-bold text-5xl">
+            {investmentOpportunity.title}
+          </h1>
+          <div className="divider"></div>
+          <p>
+            Modernizácia dopravnej infraštruktúry v Košickom kraji ponúka
+            vynikajúcu investičnú príležitosť s dlhodobou stabilitou a vysokou
+            návratnosťou. Tento projekt sa zameriava na zlepšenie regionálnej
+            prepojenosti, zvýšenie efektivity logistiky a prilákanie turistov.
+            Investícia do dopravnej siete prináša nielen ekonomické výhody, ale
+            aj významné ekologické a technologické inovácie.
+          </p>
+
+          <div className="divider py-8"></div>
+
+          <ul className="flex flex-col gap-2">
+            <li>
+              <span className="text-hover italic text-lg font-bold">
+                Výnosnosť investície:
+              </span>{" "}
+              Očakáva sa ročná návratnosť (ROI) na úrovni
+              <span className="text-hover italic text-lg"> 8-10 %</span>, čo
+              zaisťuje stabilný príjem pre investorov vďaka zvýšenej efektivite
+              logistiky a cestovného ruchu. Zlepšenie regionálnej
+            </li>
+            <li>
+              <span className="text-hover italic text-lg font-bold">
+                Zlepšenie regionálnej dostupnosti:
+              </span>{" "}
+              Modernizácia ciest a výstavba nových cyklotrás skráti čas
+              dochádzky do priemyselných zón o{" "}
+              <span className="text-hover italic text-lg">25 %</span>, čo
+              podporí rast obchodu a zlepší prepojenie s ďalšími regiónmi.
+            </li>
+          </ul>
+
+          <div className="flex justify-center mt-10">
+            <button className="text-3xl bg-tertiary py-6 px-10 rounded-lg">
+              Mám záujem investovať
+            </button>
+          </div>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
