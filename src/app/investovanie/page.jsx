7d0fe282c625491022b4d682ad2599c65015a051
Filip Hemlezi <filip.hemlezi@student.tuke.sk>
2024-09-24 14:36:09 +0200

    better form

--- src/app/investovanie/page.jsx
+++ src/app/investovanie/page.jsx
@@ -8,6 +8,7 @@ const slides = [
     title: "Kto ste?",
     questions: ["Firma", "Investor", "Startup", "Iný"],
     type: "radio",
+    backgroundImage: "/images/background3.jfif", // Replace with actual image URL
   },
   {
     title: "Ktorý sektor Vás zaujíma?",
@@ -23,6 +24,7 @@ const slides = [
       "Ekonomika",
     ],
     type: "checkbox",
+    backgroundImage: "/images/background4.jpg", // Replace with actual image URL
   },
   {
     title: "Koľko mate finančných prostriedkov?",
@@ -33,14 +35,16 @@ const slides = [
       "1 milion eur a viac",
     ],
     type: "radio",
+    backgroundImage: "/images/background5.jpg", // Replace with actual image URL
   },
   {
     title: "Aký je váš investičný horizont?",
     questions: ["1-3 roky", "3-5 rokov", "5-10 rokov", "10+ rokov"],
     type: "radio",
+    backgroundImage: "/images/background6.jpg", // Replace with actual image URL
   },
   {
-    title: "Vyberte etapu investície:?",
+    title: "Vyberte etapu investície:",
     questions: [
       "Skorý Startup",
       "Seria A",
@@ -52,28 +56,34 @@ const slides = [
       "IPO",
     ],
     type: "checkbox",
+    backgroundImage: "/images/background7.jpg" // Replace with actual image URL
   },
   {
-    title: "Vyberte risk:?",
+    title: "Vyberte risk:",
     questions: ["Nízky", "Stredný", "Vysoký"],
     type: "radio",
+    backgroundImage: "/images/background8.jpg", // Replace with actual image URL
   },
   {
-    title: "Vyberte typ účasti:?",
+    title: "Vyberte typ účasti:",
     questions: ["Hands-off", "Poradenská", "Aktívna účasť"],
     type: "radio",
+    backgroundImage: "/images/background9.jpg", // Replace with actual image URL
   },
 ];
 
 const Page = () => {
   const router = useRouter();
-
   const [activeSlide, setActiveSlide] = useState(0);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const handleNextSlide = () => {
-    setActiveSlide((prev) => prev + 1);
+    if (activeSlide < slides.length - 1) {
+      setActiveSlide((prev) => prev + 1);
+    } else {
+      setIsProcessing(true);
 
-    if (activeSlide === slides.length) {
+      // After 2 seconds, redirect to the next page
       setTimeout(() => {
         router.push("/investovanie/ponuky");
       }, 2000);
@@ -81,53 +91,27 @@ const Page = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-primary py-20">
-      {activeSlide <= slides.length ? (
-        <>
-          {slides.map((slide, index) => (
-            <Box
-              key={index}
-              slide={slide}
-              hidden={index !== activeSlide}
-              onNextClick={handleNextSlide}
-            />
-          ))}
-
-          <div
-            className={`w-[80%] max-w-xl md:max-w-3xl min-h-1/2 bg-secondary rounded-lg flex flex-col p-8 absolute ${
-              activeSlide < slides.length ? "hidden" : ""
-            }`}
-          >
-            <div className="flex-1">
-              <div className="text-white text-3xl text-center font-semibold mb-4">
-                Kontaktne údaje
-              </div>
-
-              <div className="flex flex-col gap-2 items-center">
-                <input
-                  type="text"
-                  placeholder="Meno / Názov firmy"
-                  className="input input-bordered w-full max-w-xs bg-gray-700 text-white"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Email"
-                  className="input input-bordered w-full max-w-xs bg-gray-700"
-                />
-              </div>
-            </div>
-            <div className="flex justify-end mt-4">
-              <button
-                className="bg-tertiary py-2 px-4 hover:bg-hover font-bold text-white rounded text-xl"
-                onClick={handleNextSlide}
-              >
-                Ďalej
-              </button>
-            </div>
-          </div>
-        </>
+    <div
+      className="flex justify-center items-center min-h-screen bg-primary py-20"
+      style={{
+        backgroundImage: !isProcessing
+          ? `url(${slides[activeSlide].backgroundImage})`
+          : "",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      {!isProcessing ? (
+        slides.map((slide, index) => (
+          <Box
+            key={index}
+            slide={slide}
+            hidden={index !== activeSlide}
+            onNextClick={handleNextSlide}
+          />
+        ))
       ) : (
+        // Show "processing" animation after the last slide
         <div className="flex flex-col gap-10 items-center px-10">
           <p className="text-2xl md:text-4xl text-center text-white">
             Vaše odpovede sa spracovávajú
@@ -144,7 +128,7 @@ export default Page;
 const Box = ({ slide, hidden, onNextClick }) => {
   return (
     <div
-      className={`w-[80%] max-w-xl md:max-w-3xl min-h-1/2 bg-secondary rounded-lg flex flex-col p-4 md:p-8 ${
+      className={`w-[80%] max-w-xl md:max-w-3xl min-h-1/2 bg-secondary bg-opacity-50 rounded-lg flex flex-col p-4 md:p-8 backdrop-blur-sm ${
         hidden ? "hidden" : ""
       }`}
     >
@@ -158,11 +142,6 @@ const Box = ({ slide, hidden, onNextClick }) => {
             <Chip key={index} text={question} type={slide.type} />
           ))}
         </ul>
-        <textarea
-          className="textarea textarea-bordered w-full mt-4 bg-gray-200 text-black resize-none"
-          placeholder="Niečo iné?"
-          rows={3}
-        ></textarea>
       </div>
       <div className="flex justify-end mt-4">
         <button
@@ -180,9 +159,9 @@ const Chip = ({ text, type }) => {
   return (
     <label className="bg-primary text-white px-4 py-2 rounded-lg flex items-center gap-2 cursor-pointer">
       {type === "radio" ? (
-        <input type="radio" class="peer radio" name={"filip"} />
+        <input type="radio" className="peer radio" name={"filip"} />
       ) : (
-        <input type="checkbox" class="peer checkbox" name={"filip"} />
+        <input type="checkbox" className="peer checkbox" name={"filip"} />
       )}
       {text}
     </label>
